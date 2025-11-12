d1de0a588ae68744ecf0f7fa327b8708eda9c628
MBenediktF <mbf@fassian.com>
2025-11-12 16:08:59 +0100

    fix: removed waitress

--- app.py
+++ app.py
@@ -190,6 +190,6 @@ def receive_result(username):
     command_results[username] = result
     return jsonify({"status": "Ergebnis gespeichert"})
 
-if __name__ == "__main__":
 
-    serve(app,host="0.0.0.0",port=5000)  
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
